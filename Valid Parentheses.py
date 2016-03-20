26673e1ff4cb8c3b17ca4bbcd3ef94316a5a7399
JamesG3 <emislim12@gmail.com>
2016-03-20 11:10:02 +0800

    Create Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+            L = ["[", "{", "("]
+            R = ["]", "}", ")"]
+            stack = []
+            
+            for c in s:
+                if c in L:                                              #if c is [ { (, keep append stack
+                    stack.append(c)
+                
+                if c in R:
+                    if len(stack) == 0:
+                        return False
+                    if(L.index(stack.pop()) != R.index(c)):             #compare the left part and the right part,and pop the right elements
+                        return False
+                        
+            if len(stack) > 0:                                          #if there is still element left in the stack,false
+                return False
+                
+            return True
