a0984dd09f900794fed0b16b34e2d97e0da8b8ff
JamesG3 <emislim12@gmail.com>
2016-03-26 09:33:20 +0800

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -2,17 +2,24 @@ import random                                           #a simple but time consu
 import math
 class Solution(object):
     def permute(self, nums):
-        num=nums[:]
-        tem=num[:]
-        res=[num]
-        length=len(res)
-        while(len(res)<math.factorial (len(nums))):
-            if(tem not in res):
-                res.append(tem)
-            else:
-                random.shuffle(nums)
-                tem=nums[:]
-        return res
+    res = []
+    while len(res) < math.factorial(len(nums)):
+        random.shuffle(nums)
+        if nums not in res:
+            res.append(nums[:])
+    return res
+    #def permute(self, nums):
+    #    num=nums[:]
+    #    tem=num[:]
+    #    res=[num]
+    #    length=len(res)
+    #    while(len(res)<math.factorial (len(nums))):
+    #        if(tem not in res):
+    #            res.append(tem)
+    #        else:
+    #            random.shuffle(nums)
+    #            tem=nums[:]
+    #    return res
             
         
         """
