b59792297060659e32b775ee9a8a8704c2ff5c5c
JamesG3 <emislim12@gmail.com>
2016-03-29 19:04:34 +0800

    Create Contains Duplicate II.py

---
+++ Contains Duplicate II.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        dic = {}                              # Init a dictionary,using hash
+        tem = 0
+        for i in range(0,len(nums)):
+            tem = nums[i]
+            if (tem not in dic or i-dic[tem] > k):
+                dic[tem] = i
+            else:
+                return True
+        return False
+            
+        
+    
+#   Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k. 
+    
+    
+    
+    
+#    def containsNearbyDuplicate(self, nums, k):                    #An time consuming solution
+#        if(k==0 or k==len(nums)):
+#            return False
+#        tem=[]
+#        for i in range(0,k):
+#            tem.append('index')
+#        count=0
+#        while(count<len(nums)-1):
+            
+#            for i in range(0,k):
+#                if(nums[count] in tem):
+#                    return True
+#                else:
+#                    tem[i]=nums[count]
+#                    count+=1
+#                    if(count==len(nums)):
+#                        return False
+#        return False
+            
+            
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        
