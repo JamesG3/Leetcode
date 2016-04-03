664ac2fd78414e16142cd5a83692d09198cfe5f2
JamesG3 <emislim12@gmail.com>
2016-04-03 13:33:32 +0800

    Update Add Digits.py

--- Add Digits.py
+++ Add Digits.py
@@ -10,3 +10,6 @@ class Solution(object):
             num=(num%10+num/10)
             
         return num
+         #Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+        #For example:
+        #Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it. 
