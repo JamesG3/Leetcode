833ef0435c59330bde5d775bb565bcd2b543778c
JamesG3 <emislim12@gmail.com>
2016-04-05 11:01:09 +0800

    Update Move Zeroes.py

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -22,3 +22,7 @@ class Solution(object):                  #shorter version
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+
+
+
+#Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements. 
