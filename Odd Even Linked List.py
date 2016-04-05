faeca617b9903f67834029ce9c3d90674e0cccbc
JamesG3 <emislim12@gmail.com>
2016-04-05 11:03:20 +0800

    Update Odd Even Linked List.py

--- Odd Even Linked List.py
+++ Odd Even Linked List.py
@@ -41,3 +41,9 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+
+
+#Given a singly linked list, group all odd nodes together followed by the even nodes. 
+#Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
