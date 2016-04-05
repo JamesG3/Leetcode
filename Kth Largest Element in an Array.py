b17cfb89cd6f43c03bcce8b7f8fcbe7fb13f784d
JamesG3 <emislim12@gmail.com>
2016-04-05 10:38:17 +0800

    Update Kth Largest Element in an Array.py

--- Kth Largest Element in an Array.py
+++ Kth Largest Element in an Array.py
@@ -6,4 +6,4 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element. 
+        # Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element. 
