3fcd5e934d333baa00e27519c41a1577bc73bd41
JamesG3 <emislim12@gmail.com>
2016-04-06 22:02:46 +0800

    Update Submission Details.py

--- Submission Details.py
+++ Submission Details.py
@@ -28,3 +28,25 @@ class Solution(object):
             return 500
         elif(x=='M'):
             return 1000
+#class Solution(object):             #another solution
+#    def romanToInt(self, s):
+#        count=0
+#        Dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+#        Sum=Dic[s[0]]
+        
+#        while(count<len(s)-1):
+#            if(Dic[s[count]]<Dic[s[count+1]]):
+#                Sum+=Dic[s[count+1]]-2*Dic[s[count]]
+#            else:
+#                Sum+=Dic[s[count+1]]
+#            count+=1
+#        return Sum
+#        """
+#        :type s: str
+#        :rtype: int
+#        """
+
+
+
+#Given a roman numeral, convert it to an integer.
+#Input is guaranteed to be within the range from 1 to 3999.
