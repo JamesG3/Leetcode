a21746cb3375c53b0feb411c34dd7492a432b2fa
JamesG3 <emislim12@gmail.com>
2016-04-06 21:52:31 +0800

    Update Valid Anagram.py

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -21,3 +21,8 @@ class Solution(object):
 #            return True
 #        else:
 #            return False
+
+
+
+#Given two strings s and t, write a function to determine if t is an anagram of s.
+#For example,s = "anagram", t = "nagaram", return true. = "rat", t = "car", return false. 
