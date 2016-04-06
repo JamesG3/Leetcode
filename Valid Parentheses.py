db3a6c933648fad02002fd24f69b8980f721c79f
JamesG3 <emislim12@gmail.com>
2016-04-06 21:49:40 +0800

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -21,4 +21,5 @@ class Solution(object):
 
 
 
-#
+#Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
