da548ab9b7eea5e7571d164b65401fe66e92c238
JamesG3 <emislim12@gmail.com>
2016-04-07 21:13:52 +0800

    Update Roman to Integer.py

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -15,3 +15,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+#Given a roman numeral, convert it to an integer.
+
+#Input is guaranteed to be within the range from 1 to 3999.
