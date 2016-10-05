4008088b71777f6235b519e82dc174e73a577d15
JamesG3 <emislim12@gmail.com>
2016-10-04 20:47:32 -0400

    Update Integer Replacement.py

--- Integer Replacement.py
+++ Integer Replacement.py
@@ -5,7 +5,7 @@ class Solution(object):
             if n%2 == 0:
                 n=n/2
             else:
-                if (n-1)/2 % 2 == 0:           #if (n-1)/2 % 2 == 0 and (n+1)/2 % 2 != 0:
+                if (n-1)/2 % 2 == 0:           #if (n-1)/2 % 2 == 0 and (n+1)/2 % 2 != 0:   (half of this sentence actually useless, cause (n-1)/2 %2 means that (n+1)/2 %2 !=0)
                     n-=1
                 elif (n+1)/2 % 2 == 0 and (n+1)/2 < (n-1):  #elif (n-1)/2 % 2 != 0 and (n+1)/2 % 2 == 0 and (n+1)/2 < (n-1):
                     n+=1
@@ -16,5 +16,10 @@ class Solution(object):
         """
         :type n: int
         :rtype: int
+        
+        Given a positive integer n and you can do operations as follow:
+        If n is even, replace n with n/2.
+        If n is odd, you can replace n with either n + 1 or n - 1.
+        What is the minimum number of replacements needed for n to become 1?
         """
         
