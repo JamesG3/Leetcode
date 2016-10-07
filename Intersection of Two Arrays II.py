2a777243f44c9e1eb0f9922866fc1ae9454a831d
JamesG3 <emislim12@gmail.com>
2016-10-07 16:09:25 -0400

    Update Intersection of Two Arrays II.py

--- Intersection of Two Arrays II.py
+++ Intersection of Two Arrays II.py
@@ -9,10 +9,15 @@ class Solution(object):
         return ans
                 
             
+        #using del to check every number in the nums1, if there is a same one in the nums2, then append it to ans and delete both of them from nums1 and nums2
+        #if not, only del it from nums1.
+        #when one of list is empty, end the loop.
         
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
+        
+        Given two arrays, write a function to compute their intersection.
         """
         
