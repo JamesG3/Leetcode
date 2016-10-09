baa4b53a97f7ab8e9b64c0643561ded0a6766b49
JamesG3 <emislim12@gmail.com>
2016-10-09 16:50:56 -0400

    Update Reverse Vowels of a String.py

--- Reverse Vowels of a String.py
+++ Reverse Vowels of a String.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def reverseVowels(self, s):
-        Vow="aeiouAEIOU"
-        ls=list(s)
-        tem=[]
-        n=0
-        while n!= len(ls):
+        Vow="aeiouAEIOU"                #list the possible vowels as a string
+        ls=list(s)                      #convert s into list for convenience
+        tem=[]                          #create a empty list for storage move out vowels
+        n=0                             #index for ls
+        while n!= len(ls):              #read through ls and find vowels out, save them into tem
             if ls[n] in Vow:
                 tem.append(ls[n])
-                ls[n]="aa"
+                ls[n]="aa"              #and replace them with double characters
             n+=1
         m=0
-        while len(tem)!=0:
+        while len(tem)!=0:              #replace vowels in increase order with decrease items in tem
             if ls[m]=="aa":
                 ls[m]=tem[-1]
                 del tem[-1]
@@ -22,6 +22,10 @@ class Solution(object):
             
         """
         :type s: str
+        :rtype: str
+        
+        Write a function that takes a string as input and reverse only the vowels of a string.
+        Eg. Given s = "hello", return "holle". Given s = "leetcode", return "leotcede".
         :rtype: str
         """
         
