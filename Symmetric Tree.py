45d0771e35a7e7a7b319a8c9ad86c0a33dc488c6
JamesG3 <emislim12@gmail.com>
2016-10-18 12:14:25 -0400

    Update Symmetric Tree.py

--- Symmetric Tree.py
+++ Symmetric Tree.py
@@ -7,7 +7,7 @@
 
 class Solution(object):
     def isSymmetric(self, root):
-        if root == None:
+        if root == None:                                        # For situation []
             return True
         return self.recursive(root.left, root.right)
         
@@ -15,16 +15,18 @@ class Solution(object):
     def recursive(self, left, right):
         if left != None and right != None:
             if left.val == right.val:
-                return self.recursive(left.left, right.right) and self.recursive(left.right, right.left)
+                return self.recursive(left.left, right.right) and self.recursive(left.right, right.left)    # check all points from start to leaves
             else:
                 return False
         else:
-            if left == right:
+            if left == right:                                   # check if left and right are both null
                 return True
             else:
                 return False
         """
         :type root: TreeNode
         :rtype: bool
+        
+        Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
         """
         
