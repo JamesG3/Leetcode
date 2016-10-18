229b2f3fd1b0240e546721cff008e78a1633e795
JamesG3 <emislim12@gmail.com>
2016-10-18 11:50:36 -0400

    Create Unique Paths.py

---
+++ Unique Paths.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        return math.factorial(m+n-2)/( math.factorial(m-1) * math.factorial(n-1) )
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        
