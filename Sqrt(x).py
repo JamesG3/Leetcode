d803aa41828ab7db937014ed2c1dba520b93c022
James Guo <emislim12@gmail.com>
2016-10-22 16:39:03 -0400

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def mySqrt(self, x):
+        return int(math.sqrt(x))
+        """
+        :type x: int
+        :rtype: int
+        
+        Implement int sqrt(int x).
+        Compute and return the square root of x.
+        """
+        
