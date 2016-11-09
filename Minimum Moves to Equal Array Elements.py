736c1d66730a8ad25845e1de31e50e165634edd7
James Guo <emislim12@gmail.com>
2016-11-08 19:02:30 -0500

    Update Minimum Moves to Equal Array Elements.py

--- Minimum Moves to Equal Array Elements.py
+++ Minimum Moves to Equal Array Elements.py
@@ -2,8 +2,27 @@ class Solution(object):
     def minMoves(self, nums):
         
         step=0
+        nums.sort()
+        for n in range(len(nums)-1,-1,-1):
+            if nums[n] == nums[0]:
+                break
+            else:
+                step+=nums[n]-nums[0]
+            
+        return step
+            
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+"""
+# brute force solution
+
+class Solution(object):
+    def minMoves(self, nums):
+        
+        step=0
         while nums.count(nums[0])!=len(nums):
-            nums.sort()
             temmax=max(nums)
             temmin=min(nums)
             temstep=temmax-temmin
@@ -20,7 +39,4 @@ class Solution(object):
             
         return step
             
-        """
-        :type nums: List[int]
-        :rtype: int
         """
