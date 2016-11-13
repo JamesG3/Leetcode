dcaa69f736fff0733d0155b9bd47ec08e09561a4
James Guo <emislim12@gmail.com>
2016-11-12 20:11:36 -0500

    Update Find All Anagrams in a String.py

--- Find All Anagrams in a String.py
+++ Find All Anagrams in a String.py
@@ -1,4 +1,39 @@
-class Solution(object):
+from collections import Counter
+#Could be faster if add a condition: if(next letter in s not belongs to p, jump the entire window to the next next letter)
+class Solution(object):                     #Sliding window solution
+    def findAnagrams(self, s, p):
+        ans=[]
+        
+        S=Counter(s[:len(p)-1])
+        P=Counter(p)
+        
+        for n in range(len(p)-1,len(s)):
+            S[s[n]]+=1
+            if S == P:
+                ans.append(n-len(p)+1)
+            S[s[n-len(p)+1]] -= 1
+            if S[s[n-len(p)+1]] == 0:
+                del S[s[n-len(p)+1]]
+        return ans
+    
+    
+        """
+        Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+        Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+        The order of output does not matter.
+        
+        EXAMPLE:
+        Input:
+        s: "cbaebabacd" p: "abc"
+        Output:
+        [0, 6]
+        
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        
+    """
     def findAnagrams(self, s, p):       #brute force solution, check every character and several bits after it
                                         #cause a TLE error
         s=list(s)
@@ -27,9 +62,5 @@ class Solution(object):
                 
         return ans
         
-        """
-       
-        :type s: str
-        :type p: str
-        :rtype: List[int]
+
         """
