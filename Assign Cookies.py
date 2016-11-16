880f2ee2bfc8b12a8d7b75b5ae90829033c8706a
James Guo <emislim12@gmail.com>
2016-11-15 23:24:44 -0500

    Update Assign Cookies.py

--- Assign Cookies.py
+++ Assign Cookies.py
@@ -11,7 +11,18 @@ class Solution(object):
                 del g[0]
                 counter+=1
         return counter
+        
         """
+        Example:
+        Input: [1,2,3], [1,1]
+        Output: 1
+        Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
+        And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+        You need to output 1.
+        
+        You may assume the greed factor is always positive. 
+        You cannot assign more than one cookie to one child.
+        
         :type g: List[int]
         :type s: List[int]
         :rtype: int
