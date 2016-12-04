594ff8214fe9cd676c5c74867cf16e662bcc20e4
James Guo <emislim12@gmail.com>
2016-12-04 12:00:12 -0500

    Update Rotate Array.py

--- Rotate Array.py
+++ Rotate Array.py
@@ -12,6 +12,35 @@ class Solution(object):
         
 
 """
+
+    #java reverse solution
+    #use O(n) time and O(1) extera space(no extera space)
+    #rotate 3 times
+    #1 2 3 4 5 6 7
+    #7 6 5 4 3 2 1
+    #5 6 7 4 3 2 1
+    #5 6 7 1 2 3 4
+    
+    public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    }
+
+
+
     #TLE solution (usign O(1) extra space)
     def rotate(self, nums, k):
         
