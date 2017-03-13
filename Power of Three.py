ad04638dae7d1ae1de95117f0450b7ae3bc0d2cb
James Guo <emislim12@gmail.com>
2017-03-12 22:22:22 -0400

    Create Power of Three.py

---
+++ Power of Three.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        
+        return n>0 and 1162261467%n == 0    #0 is not power of three, 1162261467 is the largest int number which is also power of three
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
+        
+        #Given an integer, write a function to determine if it is a power of three.
+        
+       
