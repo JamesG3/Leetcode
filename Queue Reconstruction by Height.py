a10a21c4a169b06236fc3eb1ab6b72541af7663f
James Guo <emislim12@gmail.com>
2017-03-25 15:21:30 -0400

    Create Queue Reconstruction by Height.py

---
+++ Queue Reconstruction by Height.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def reconstructQueue(self, people):
+        res = []
+        length = len(people)
+        if length == 0:
+            return []
+        
+        people = sorted(people)[::-1]
+        M = people[0][0]
+        
+        i = 0
+        
+        while i != length:                      #initialize the first part
+            if people[i][0] == M:
+                res = [people[i]] + res
+                i+=1
+            else:
+                M = people[i][0]
+                break
+            
+        tem = []
+        while i != length:                      #for each group of people with the same height, append them into a tem list
+            if people[i][0] == M:
+                tem.append(people[i])
+            else:
+                while len(tem) != 0:
+                    ap = tem.pop()              # insert people into array with asc order
+                    index = ap[1]
+                    res = res[0:index] + [ap] + res[index:]
+                M = people[i][0]
+                tem.append(people[i])
+            i += 1
+            
+        while len(tem) != 0:                # insert the last part
+            ap = tem.pop()
+            index = ap[1]
+            res = res[0:index] + [ap] + res[index:]
+                
+        return res
+        
+        
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        #Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+        #The number of people is less than 1,100.
