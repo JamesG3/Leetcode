fca17bc8fb4530fecbd3e6b967831353613495a6
James Guo <emislim12@gmail.com>
2017-06-25 17:38:06 -0400

    Update Count Primes.py

--- Count Primes.py
+++ Count Primes.py
@@ -20,10 +20,24 @@ class Solution(object):
             
         return count
         
-                
+        
+        
         """
-        :type n: int
-        :rtype: int
+# this solution will be a bit faster
+class Solution(object):
+    def countPrimes(self, n):
+        if n <= 2:
+            return 0
+        res = [1] * n
+        res[0] = res[1] = 0
+        
+        for i in xrange(2, n):
+            if res[i] == 1:
+                for j in xrange(2, (n-1)//i+1):
+                    res[i*j] = 0
+                    
+        return sum(res)
+        
         """
         
         #Count the number of prime numbers less than a non-negative number, n.
