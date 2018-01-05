b4845d0005129a725cd88d7e2677decd6e609d3d
James Guo <emislim12@gmail.com>
2018-01-05 09:46:51 +0800

    Create Wiggle Sort.py

---
+++ Wiggle Sort.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def wiggleSort(self, nums):
+        for i in xrange(len(nums)):
+            if i == 0:
+                continue
+            if i%2 == 0:
+                if nums[i] > nums[i-1]:
+                    nums[i], nums[i-1] = nums[i-1], nums[i]
+            else:
+                if nums[i] < nums[i-1]:
+                    nums[i], nums[i-1] = nums[i-1], nums[i]
+            
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        
+        # Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+        # For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+        
