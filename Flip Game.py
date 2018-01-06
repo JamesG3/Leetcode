aa80fc7e415991e75a9ad9c5b29d43d44be0c357
James Guo <emislim12@gmail.com>
2018-01-06 21:15:10 +0800

    Create Flip Game.py

---
+++ Flip Game.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def generatePossibleNextMoves(self, s):
+        if len(s) < 2:
+            return []
+        if list(set(s)) == ['-']:
+            return []
+        
+        res = []
+        
+        for i in xrange(len(s)-1):
+            if s[i] == s[i+1]:
+                if s[i] == "+":
+                    res.append(s[:i] + '--' + s[i+2:])
+
+        return res
+                    
+            
+        
+        
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        
+        # You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". 
+        # The game ends when a person can no longer make a move and therefore the other person will be the winner.
+        # Write a function to compute all possible states of the string after one valid move.
