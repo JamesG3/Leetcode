ac0a35cea3ab4bddb3412b929ef6b9cb7ed3f952
James Guo <emislim12@gmail.com>
2018-02-03 22:39:30 -0500

    Update Hamming Distance.py

--- Hamming Distance.py
+++ Hamming Distance.py
@@ -1,27 +1,29 @@
 class Solution(object):
     def hammingDistance(self, x, y):
+        return bin(x^y).count('1')
         
-        if x == y:
-            return 0
         
-        x_bin = bin(x)[2:]
-        y_bin = bin(y)[2:]          #transform to binary
+#         if x == y:
+#             return 0
         
-        x_len = len(x_bin)
-        y_len = len(y_bin)
+#         x_bin = bin(x)[2:]
+#         y_bin = bin(y)[2:]          #transform to binary
         
-        if x_len > y_len:           #fix length
-            y_bin = '0'*(x_len - y_len) + y_bin
-        elif y_len > x_len:
-            x_bin = '0'*(y_len - x_len) + x_bin
+#         x_len = len(x_bin)
+#         y_len = len(y_bin)
         
-        counter = 0
-        index = 0
-        while index != max(x_len, y_len):       #compare each digit
-            if x_bin[index] != y_bin[index]:
-                counter+=1
-            index+=1
-        return counter
+#         if x_len > y_len:           #fix length
+#             y_bin = '0'*(x_len - y_len) + y_bin
+#         elif y_len > x_len:
+#             x_bin = '0'*(y_len - x_len) + x_bin
+        
+#         counter = 0
+#         index = 0
+#         while index != max(x_len, y_len):       #compare each digit
+#             if x_bin[index] != y_bin[index]:
+#                 counter+=1
+#             index+=1
+#         return counter
         
                                         #To save time, we can compare from the fist digit of the shorter one, then add this number with the count of character '1' of the rest part.
         """
