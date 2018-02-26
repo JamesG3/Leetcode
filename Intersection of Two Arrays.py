7b445287bb26f57172bfc6ce27af5cc12aee70b7
James Guo <emislim12@gmail.com>
2018-02-26 17:04:11 -0500

    Update Intersection of Two Arrays.py

--- Intersection of Two Arrays.py
+++ Intersection of Two Arrays.py
@@ -1,18 +1,10 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
-        nums1=set(nums1)
-        nums2=set(nums2)
-        r=[]
-        for n in nums1:
-            if n in nums2:
-                r.append(n)
-            else:
-                continue
-        return r
+        return list(set(nums1) & set(nums2))
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
-        Given two arrays, write a function to compute their intersection.
+        Given two arrays, write a function to compute their intersection.(find common elements)
         Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
         """
