704c80d588d2c999ce6f12d3228347dc25e99d6b
James Guo <emislim12@gmail.com>
2018-02-27 16:12:22 -0500

    Update Number Complement.py

--- Number Complement.py
+++ Number Complement.py
@@ -1,4 +1,10 @@
 class Solution(object):
+    # one line solution
+    def findComplement(self, num):
+        return num ^ (2**(len(bin(num))-2)-1)
+    
+    
+    
     def findComplement(self, num):
         multiple = math.log(num, 2)
         ceil = math.floor(multiple) + 1
