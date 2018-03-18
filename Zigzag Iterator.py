caaae3968517c07d22239ec704a49fc716deba4d
James Guo <emislim12@gmail.com>
2018-03-17 20:40:16 -0400

    Update Zigzag Iterator.py

--- Zigzag Iterator.py
+++ Zigzag Iterator.py
@@ -55,6 +55,43 @@ class ZigzagIterator(object):
 
 
 
+# queue solution, use more space and slow, but easy to understand
+class ZigzagIterator(object):
+    def __init__(self, v1, v2):
+        self.q = [v for v in (v1,v2) if v]
+        """
+        Initialize your data structure here.
+        :type v1: List[int]
+        :type v2: List[int]
+        """
+        
+
+    def next(self):
+        v = self.q.pop(0)
+        res = v.pop(0)
+        if v:
+            self.q.append(v)
+        return res
+        
+        
+        """
+        :rtype: int
+        """
+        
+
+    def hasNext(self):
+        if self.q:
+            return True
+        return False
+        """
+        :rtype: bool
+        """
+        
+
+# Your ZigzagIterator object will be instantiated and called as such:
+# i, v = ZigzagIterator(v1, v2), []
+# while i.hasNext(): v.append(i.next())
+
 
 
 # Given two 1d vectors, implement an iterator to return their elements alternately.
