998a683928935c2a98c51c14180146a86eb90b09
James Guo <emislim12@gmail.com>
2018-03-20 12:03:47 -0400

    Update Queue Reconstruction by Height.py

--- Queue Reconstruction by Height.py
+++ Queue Reconstruction by Height.py
@@ -1,4 +1,15 @@
 class Solution(object):
+    # concise solution, O(n^2)
+    def reconstructQueue(self, people):
+        people = sorted(people, key=lambda x: x[1])
+        people = sorted(people, key=lambda x: -x[0])
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
+    
+    
+    
     def reconstructQueue(self, people):
         res = []
         length = len(people)
