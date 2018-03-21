630f840338658cc24205663a38ca47c6d01210da
James Guo <emislim12@gmail.com>
2018-03-21 16:55:13 -0400

    Update Missing Number.py

--- Missing Number.py
+++ Missing Number.py
@@ -1,14 +1,24 @@
 class Solution(object):
+    # bit manipulation solution , XOR
     def missingNumber(self, nums):
-        ans=(max(nums)+min(nums))*(len(nums)+1)/2-sum(nums)
-        if(ans in nums):
-            if(0 in nums):
-                return max(nums)+1
-            else:
-                return 0
-        return ans
+        length = len(nums)
+        for i,n in enumerate(nums):
+            length ^= i^n
+        
+        return length
+        
+    
+    # sum solution
+    # def missingNumber(self, nums):
+    #     length = len(nums)
+    #     return (length*(length+1))/2 - sum(nums)
+    
+    
         """
         :type nums: List[int]
         :rtype: int
         """
-        #Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array. 
+        
+        
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
