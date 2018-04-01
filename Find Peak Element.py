7d1636c9d8b6db68c27c45deef4755244b67f970
James Guo <emislim12@gmail.com>
2018-04-01 16:26:17 -0400

    Update Find Peak Element.py

--- Find Peak Element.py
+++ Find Peak Element.py
@@ -1,8 +1,20 @@
 class Solution(object):
+    # binary search solution, O(lgn), O(1)
     def findPeakElement(self, nums):
-        return nums.index(max(nums))
+        l, r = 0, len(nums)-1
+        
+        while l<r:
+            m = (l+r) / 2
+            if nums[m] < nums[m+1]:
+                l = m+1
+            else:
+                r = m
+        return l
+        
+        
+        
         """
         :type nums: List[int]
         :rtype: int
         """
-        #Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+        # find the peak element and return it's index, in O(logn)
