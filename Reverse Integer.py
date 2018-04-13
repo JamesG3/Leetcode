9e1ad06cc1a34c2a612b1ba734c4bd14adbc157d
James Guo <emislim12@gmail.com>
2018-04-12 23:21:08 -0400

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,19 +1,21 @@
-class Solution(object):
+class Solution:
     def reverse(self, x):
-        b=list(str(x))
-        r=""
-        if b[0] == '-':
-            r+='-'
-            b=b[1:len(b)]
-        b.reverse()
-        for n in b:
-            r+=n
-        if -2147483647<int(r)<2147483647:
-            return int(r)
-        else:
-            return 0
+        s = cmp(x, 0)
+        res = 0
+        
+        x = s*x
+        while x > 0:
+            res = res*10 + x%10
+            x /= 10
             
+        res = s*res
+        return res if -2**31 <= res <= 2**31-1 else 0
         """
         :type x: int
         :rtype: int
         """
+        
+        
+        
+        # Given a 32-bit signed integer, reverse digits of an integer.
+
