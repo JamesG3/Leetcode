b3fe9843408035718fbee078d4c2c3d4b76147b3
James Guo <emislim12@gmail.com>
2018-04-22 22:39:47 -0400

    Update Search Insert Position.py

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -1,18 +1,26 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        if target in nums:
-            return nums.index(target)
-        else:
-            for n in nums:
-                if(n>target):
-                    return nums.index(n)
-            if(target<nums[0]):
-                return 0
-            elif(target>nums[-1]):
-                return len(nums)
+        p = 0
+        length = len(nums)
+        
+        if length == 0 or target <= nums[0]:
+            return 0
+        
+        if target > nums[-1]:
+            return length
+
+        while not (nums[p] <= target <= nums[p+1]):
+            p+=1
+            
+        return p+1
+            
+        
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        #Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+        
+        
+        # Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+        # You may assume no duplicates in the array.
