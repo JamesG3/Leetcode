211f6ec598db02d5462c9bd5cafbebdd28584ce6
James Guo <emislim12@gmail.com>
2018-04-28 23:02:29 -0400

    Update Length of Last Word.py

--- Length of Last Word.py
+++ Length of Last Word.py
@@ -1,26 +1,18 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        s=list(s)
-        s.reverse()
-        count=0
-        if s==[]:
-            return 0
-        else:
-            for item in s:
-                if item != " ":
-                    count+=1
-                elif count!=0:
-                    break
-        return count
+        s = s.split(" ")
+        for i in xrange(len(s)):
+            if len(s[~i]) != 0:
+                return len(s[~i])
             
+        return 0
         """
         :type s: str
         :rtype: int
-        Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
-        If the last word does not exist, return 0.
-        Note: A word is defined as a character sequence consists of non-space characters only.
-        For example, 
-        Given s = "Hello World",
-        return 5.
         """
+        
+        
+        # Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+        # If the last word does not exist, return 0.
+        # Note: A word is defined as a character sequence consists of non-space characters only.
         
