9c84696d7ccad0e4a601b39ffb1bdf71ea03af25
James Guo <emislim12@gmail.com>
2018-05-02 11:41:56 -0400

    Update Add Binary.py

--- Add Binary.py
+++ Add Binary.py
@@ -1,4 +1,31 @@
 class Solution(object):
+    # bit solution
+    def addBinary(self, a, b):
+        carry = 0
+        alen = len(a)
+        blen = len(b)
+        res = ""
+        
+        if alen < blen:
+            a = "0" * (blen-alen) + a
+            alen = blen
+        elif blen < alen:
+            b = "0" * (alen-blen) + b
+            blen = alen
+            
+        for j in xrange(alen):
+            res = str((int(a[~j]) + int(b[~j]) + carry) % 2) + res
+            carry = int((int(a[~j]) + int(b[~j]) + carry) / 2)
+            
+        if carry:
+            res = "1" + res
+            
+        return res
+        
+    
+    
+    
+    # bin solution
     def addBinary(self, a, b):
         c=int(a,2)+int(b,2)
         c=bin(int(str(c),10))[2:]
