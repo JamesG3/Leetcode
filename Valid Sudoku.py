9fee13ef0ecfaf8d6dd2fb7333473af71b6b4ccb
James Guo <emislim12@gmail.com>
2018-05-07 21:31:49 -0400

    Update Valid Sudoku.py

--- Valid Sudoku.py
+++ Valid Sudoku.py
@@ -1,76 +1,58 @@
 class Solution(object):
-    def isValidSudoku(self, board):                 #brute force
-        orig = ['1','2','3','4','5','6','7','8','9','0']
-        compare = copy.deepcopy(orig)
-        index = len(board)
+    def isValidSudoku(self, board):
         
-        for line in board:                  #check every row
-            for n in line:
-                if n != '.':
-                    if n in compare:
-                        compare.remove(n)
-                    else:
-                        return False
-            
-            compare = copy.deepcopy(orig)
-        
-        compare = copy.deepcopy(orig)
-            
-        for j in xrange(index):             #check every column
-            for i in xrange(index):
-                if board[i][j] != ".":
-                    if board[i][j] in compare:
-                        compare.remove(board[i][j])
-                    else:
-                        return False
-            print compare
-            compare = copy.deepcopy(orig)
-        
-        compare = copy.deepcopy(orig)
-        i = 0
-        j = 0
-        jmark = j
-        imark = i
-        
-        while i != index:               #check every 9*9 unit
-            if j==index and (i+1)%3==0:
-                j=0
-                jmark=j
-                i+=1
-                imark=i
-                compare=copy.deepcopy(orig)
-                continue
-            
-            if j==index and (i+1)%3!=0:
-                j=jmark
-                i+=1
-                
-            if j != jmark and j%3 == 0 and (i+1)%3!=0:
-                i+=1
-                j=jmark
-                
-            if j != jmark and j%3 == 0 and (i+1)%3==0:
-                jmark=j
-                i=imark
-                compare=copy.deepcopy(orig)
-                
-            if board[i][j] != '.':
-                
-                if board[i][j] in compare:
-                    compare.remove(board[i][j])
+        for i in xrange(9):
+            set1 = set()
+            for j in xrange(9):
+                num = board[i][j]
+                if num == '.':
+                    continue
+                if num not in set1:
+                    set1.add(num)
                 else:
                     return False
-            j+=1
+                
+        for j in xrange(9):
+            set1 = set()
+            for i in xrange(9):
+                num = board[i][j]
+                if num == '.':
+                    continue
+                if num not in set1:
+                    set1.add(num)
+                else:
+                    return False
+                
+        for i in xrange(3):
+            for j in xrange(3):
+                set1 = set()
+                for k in xrange(9):
+                    m, n = k/3 + i*3, k%3 + j*3
+                    num = board[m][n]
+                    if num == '.':
+                        continue    
+                    if num not in set1:
+                        set1.add(board[m][n])
+                        
+                    else:
+                        return False
                     
- 
         return True
-
                     
-            
+                   
         
         """
         :type board: List[List[str]]
         :rtype: bool
         """
         
-        #A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+        # Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+        # Each row must contain the digits 1-9 without repetition.
+        # Each column must contain the digits 1-9 without repetition.
+        # Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+        
+        # The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+
+        
