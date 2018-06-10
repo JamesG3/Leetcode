a55b1abf93fbae79a1f1e922c387463dba976512
James Guo <emislim12@gmail.com>
2018-06-09 20:45:24 -0400

    Update Insert Delete GetRandom O(1).py

--- Insert Delete GetRandom O(1).py
+++ Insert Delete GetRandom O(1).py
@@ -1,51 +1,61 @@
-import random
+# O(1) time solution with O(n) space
 
 class RandomizedSet(object):
 
     def __init__(self):
         self.dic = {}
-        self.length = 0
+        self.l = []
+        self.size = 0
         """
         Initialize your data structure here.
         """
         
 
     def insert(self, val):
-        if val not in self.dic:
-            self.dic[val] = val
-            self.length += 1
-            return True
-        else:
-            return False
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         :type val: int
         :rtype: bool
         """
         
+        if val not in self.dic:
+            self.dic[val] = self.size
+            self.size += 1
+            self.l.append(val)
+            return True
+        return False
+        
 
     def remove(self, val):
-        if val in self.dic:
-            self.length -= 1
-            del self.dic[val]
-            return True
-        else:
-            return False
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         :type val: int
         :rtype: bool
         """
         
+        if val in self.dic:
+            lastNum = self.l[self.size-1]
+            if val == lastNum:
+                del self.dic[val]
+                self.l.pop()
+            else:
+                lastIndex = self.dic[lastNum]
+                self.l[lastIndex], self.l[self.dic[val]] = self.l[self.dic[val]], self.l[lastIndex]
+                self.dic[lastNum] = self.dic[val]
+                del self.dic[val]
+                self.l.pop()
+            self.size -= 1
+            return True
+        return False
+        
 
     def getRandom(self):
-        return self.dic.values()[random.randrange(self.length)]
         """
         Get a random element from the set.
         :rtype: int
         """
+        return self.l[random.randrange(self.size)]
         
-
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
