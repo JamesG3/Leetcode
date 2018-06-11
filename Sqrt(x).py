6881783811a0ce1ba1fa628e751957f08035d2cc
James Guo <emislim12@gmail.com>
2018-06-10 20:10:29 -0400

    Update Sqrt(x).py

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,11 +1,19 @@
 class Solution(object):
+    
+    # Newton's method
     def mySqrt(self, x):
-        return int(math.sqrt(x))
+        r = x
+        
+        while r**2 > x:
+            r = (r + x/r) / 2
+        return r
+            
+        
         """
         :type x: int
         :rtype: int
-        
-        Implement int sqrt(int x).
-        Compute and return the square root of x.
         """
         
+        # Implement int sqrt(int x).
+        # Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+        # Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
