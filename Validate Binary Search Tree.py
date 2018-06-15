20a8abdc9e83e14cef36fb89d30755337e6ef02e
James Guo <emislim12@gmail.com>
2018-06-15 11:58:58 -0400

    Update Validate Binary Search Tree.py

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -5,6 +5,34 @@
 #         self.left = None
 #         self.right = None
 
+
+# recursive solution, O(n) time, O(1) space
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        return self.helper(root, float('-Inf'), float('Inf'))
+        
+    def helper(self, node, curmin, curmax):
+        if not node:
+            return True
+        if not (curmin < node.val < curmax):
+            return False
+        return self.helper(node.left, curmin, node.val) and self.helper(node.right, node.val, curmax)
+
+       
+
+# using in-order tree traversal, O(n) time, O(n) space
 class Solution(object):
     def isValidBST(self, root):
         sort=[]
@@ -23,10 +51,5 @@ class Solution(object):
         sort.append(root.val)
         self.inorderTraversal(root.right, sort)
         
-        
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
 
         #Given a binary tree, determine if it is a valid binary search tree (BST).
