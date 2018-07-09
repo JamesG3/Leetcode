1a25c65541b2f3b44eb4cc9357dd8f798efd6090
James Guo <emislim12@gmail.com>
2018-07-08 21:33:48 -0700

    Update Add Digits.py

--- Add Digits.py
+++ Add Digits.py
@@ -4,12 +4,10 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        if(num==0):
+        if num == 0:
             return 0
-        while(num>=10):
-            num=(num%10+num/10)
-            
-        return num
-         #Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
-        #For example:
-        #Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it. 
+        return num%9 if num%9 else 9
+        
+        
+        # Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
