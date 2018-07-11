4e20156814ca57547524c85323e274872095d53c
James Guo <emislim12@gmail.com>
2018-07-10 20:55:45 -0700

    Update Kth Largest Element in an Array.py

--- Kth Largest Element in an Array.py
+++ Kth Largest Element in an Array.py
@@ -6,4 +6,21 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        
+        
+        
+       # heap solution, slower
+    def findKthLargest(self, nums, k):
+        length = len(nums)
+        if not length or k > length:
+            return -1
+        
+        heap = []
+        for n in nums:
+            heappush(heap, -n)
+            
+        for i in xrange(k-1):
+            heappop(heap)
+            
+        return -heappop(heap)
         # Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element. 
