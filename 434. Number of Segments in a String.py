61755d7ce30a12ffcdfbf1a4a764c0e682bec9cc
James Guo <emislim12@gmail.com>
2019-06-26 23:22:04 -0700

    Update and rename Number of Segments in a String.py to 434. Number of Segments in a String.py

--- 434. Number of Segments in a String.py
+++ 434. Number of Segments in a String.py
@@ -1,5 +1,10 @@
 class Solution(object):
     def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        Solution: Usng counter
+        """
         s = s.strip()
         if len(s) == 0:         #edge case: ''
             return 0
@@ -15,10 +20,14 @@ class Solution(object):
                 mark = 0
                 
         return counter+1
+    
+    
         """
         :type s: str
         :rtype: int
+        Solution: One liner
         """
+        return len([1 for i in s.split(" ") if i != ""])
         
         #Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
         #Please note that the string does not contain any non-printable characters.
