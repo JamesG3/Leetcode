72ff1bc33e6593995c1ee247cd800fab56cc58e8
James Guo <emislim12@gmail.com>
2019-07-07 22:48:53 -0700

    Update and rename Sum of Two Integers(bug).py to 371. Sum of Two Integers.py

---
+++ 371. Sum of Two Integers.py
@@ -0,0 +1,19 @@
+import ctypes
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        Solution: Bitwise operation, using ctypes to force float32
+        """
+        if b == 0:
+            print a
+            return a
+        
+        else:
+            carry = ctypes.c_int32(a&b)
+            return self.getSum(a^b, carry.value<<1)
+        
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
