ce1a1c826cb076b9848ae72a49d9615bdec90004
James Guo <emislim12@gmail.com>
2019-12-28 14:07:54 -0800

    Update 1038. Binary Search Tree to Greater Sum Tree.py

--- 1038. Binary Search Tree to Greater Sum Tree.py
+++ 1038. Binary Search Tree to Greater Sum Tree.py
@@ -1,32 +1,38 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def bstToGst(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        Solution: Tree traverse (reversed inorder)
-        Time: O(n)
-        Space: O(1)
-        """
-        self.sum = 0
-        self.traverse(root)
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.accu_sum = 0
+        self.helper(root)
         return root
         
-    def traverse(self, node):
+        
+    def helper(self, node):
         if not node:
             return
         
-        self.traverse(node.right)
-        tmp = node.val
-        node.val += self.sum
-        self.sum += tmp
-        self.traverse(node.left)
+        self.helper(node.right)
+        node_val = node.val
+        node.val += self.accu_sum
+        self.accu_sum += node_val
+        self.helper(node.left)
         
         
-# Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.val.
+'''
+Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.val.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Solution: Right-first in order traverse (desc order BST traverse)
+Time: O(n)
+Space: O(1)
+'''
