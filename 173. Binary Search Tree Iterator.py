d8da5e55dbc3150702bb9cfcd3eb4bc040bba83f
James Guo <emislim12@gmail.com>
2020-04-08 22:27:56 -0700

    Update 173. Binary Search Tree Iterator.py

--- 173. Binary Search Tree Iterator.py
+++ 173. Binary Search Tree Iterator.py
@@ -1,52 +1,59 @@
-# Definition for a  binary tree node
-# class TreeNode(object):
+# Definition for a binary tree node.
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class BSTIterator(object):
-    def __init__(self, root):
-        self.stack = []
-        while root:                     #append the left side node in the stack
-            self.stack.append(root)
-            root = root.left
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.itator = self.traverse(root)
+        try:
+            self.cur = self.itator.__next__()
+        except:
+            self.cur = None
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        cur = self.cur
+        try:
+            self.cur = self.itator.__next__()
+        except:
+            self.cur = None
+        return cur
+        
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return self.cur != None
             
-        """
-        :type root: TreeNode
-        Solution: Stack
-        Time: O(1) sometimes O(h)
-        Spacce: O(h)
-        """
         
-
-    def hasNext(self):
-        return len(self.stack)!=0
+    def traverse(self, node):
+        if not node:
+            return
         
-        """
-        :rtype: bool
-        """
-        
-
-    def next(self):
-        currentnode = self.stack.pop()    #pop the current node
-        r = currentnode.right
-        while r:                          #append the next several nodes in descending order
-            self.stack.append(r)
-            r = r.left
-        return currentnode.val            #output the current node's val
-        
-        """
-        :rtype: int
-        """
-        
-
-# Your BSTIterator will be called like this:
-# i, v = BSTIterator(root), []
-# while i.hasNext(): v.append(i.next())
+        yield from self.traverse(node.left)
+        yield node.val
+        yield from self.traverse(node.right)
 
 
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
 
-#Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
-#Calling next() will return the next smallest number in the BST.
-#Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+'''
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+Calling next() will return the next smallest number in the BST.
+next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+You may assume that next() call will always be valid, that is, there will be at least a next smallest number in the BST when next() is called.
+
+Solution: Tree traversal, BST, generator
+Time: O(1) avg
+Space: O(h) Memory
+'''
