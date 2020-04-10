3c25b127bc3450908466dea47e763d19bb0ee44d
James Guo <emislim12@gmail.com>
2020-04-09 22:55:44 -0700

    Update 340. Longest Substring with At Most K Distinct Characters.py

--- 340. Longest Substring with At Most K Distinct Characters.py
+++ 340. Longest Substring with At Most K Distinct Characters.py
@@ -1,43 +1,32 @@
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        length = len(s)
-        max_len = 0
+        cnter = {}
+        p1, p2 = 0, 0
+        res = 0
+        cur_length = 0
         
         if k == 0:
             return 0
         
-        if length < k:
-            return length
+        while p2 < len(s):
+            cnter[s[p2]] = cnter.get(s[p2], 0) + 1
+            cur_length += 1
+            p2 += 1
+            
+            if len(cnter) > k:
+                res = max(res, cur_length-1)
+                while len(cnter) > k:
+                    cnter[s[p1]] -= 1
+                    cur_length -= 1
+                    if cnter[s[p1]] == 0:
+                        del cnter[s[p1]]
+                    p1 += 1
         
-        dic = {}
-        l, r = 0, 0
-        max_len = 0
-        while r < length:
-            if s[r] in dic:
-                dic[s[r]] = r
-            else:
-                if len(dic) == k:
-                    max_len = max(max_len, r - l)
-                    l = self.remove_leftmost(dic)
-                dic[s[r]] = r
-            r += 1
+        return max(cur_length, res)
                     
-        return max(max_len, r - l)
-    
-    
-    def remove_leftmost(self, dic):
-        rev_dic = {v:k for k,v in dic.items()}
-        leftmost = min(rev_dic)
-        del dic[rev_dic[leftmost]]
-        return leftmost + 1
-    
-    
 '''
 Given a string, find the length of the longest substring T that contains at most k distinct characters.
 
-Solution:   Two pointers, sliding window
-            exactly same solution as 159. Longest Substring with At Most Two Distinct Characters
-            but need to do some specal handling when (k == 0)
-            
-Time, Space = O(n)
-'''
+Solution: Sliding window, hash table, two pointers
+Time, Space: O(n)
+'''                
