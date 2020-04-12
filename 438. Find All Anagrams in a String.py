4eaf8110a5c80c239b5cc17702cc5218be4b0f76
James Guo <emislim12@gmail.com>
2020-04-11 21:11:56 -0700

    Update 438. Find All Anagrams in a String.py

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -1,37 +1,44 @@
-from collections import Counter
-
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        p_cnt = Counter(p)
-        s_len = len(s)
-        p_len = len(p)
+        pdic = {}
+        for c in p:
+            pdic[c] = pdic.get(c, 0) + 1
+        
         res = []
+        cur_res = 0
+        cur_dic = {}
+        for i, c in enumerate(s):
+            if cur_dic == pdic:
+                res.append(cur_res)
+                cur_dic[s[cur_res]] -= 1
+                cur_res += 1
+            
+            if c not in pdic:
+                cur_dic = {}
+                cur_res = i + 1
+            else:
+                if pdic[c] == cur_dic.get(c, 0):
+                    while s[cur_res] != c:
+                        cur_dic[s[cur_res]] -= 1
+                        cur_res += 1
+                    cur_res += 1
+                    # cur_dic[c] -= 1
+            
+                else:
+                    cur_dic[c] = cur_dic.get(c, 0) + 1
         
-        if s_len < p_len:
-            return []
-        
-        sub_s = s[:p_len]
-        cur_cnt = Counter(sub_s)
-        if cur_cnt == p_cnt:
-            res.append(0)
-        for i in range(s_len - p_len):
-            cur_cnt[s[i]] -= 1
-            if cur_cnt[s[i]] == 0:
-                del cur_cnt[s[i]]
-                
-            cur_cnt[s[p_len + i]] += 1
-                
-            if cur_cnt == p_cnt:
-                res.append(i + 1)
-        
+        if cur_dic == pdic:
+            res.append(cur_res)
+            
         return res
-
     
 '''
 Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
 Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
 The order of output does not matter.
 
-Solution: Hashtable, Counter, be careful about the optimization
-Space, Time: O(n)
+Solution: Hash table, sliding window
+Time: O(n)
+Space: O(n)
 '''
+        
